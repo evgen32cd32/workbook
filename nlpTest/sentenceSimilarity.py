5d8dc5a67f81dc2ebf193e9befc25722eaadad73
Evgeniy Khvatov <evgen32cd32@gmail.com>
2022-05-30 20:28:49 +0300

    changed output

--- nlpTest/sentenceSimilarity.py
+++ nlpTest/sentenceSimilarity.py
@@ -44,23 +44,35 @@ svd = pd.DataFrame(pca.fit_transform(df)).transpose();
 
 
 #distance
-res = pd.DataFrame();
+res = [];
 for i in range(len(svd.columns)):
-	res[i] = np.NaN;
-	res.loc[i] = np.NaN;
 	for j in range(i):
-		res.loc[i,j] = np.sqrt(np.power(svd[i] - svd[j],2).sum());
-		res.loc[j,i] = res.loc[i,j];
+		res.append([np.sqrt(np.power(svd[i] - svd[j],2).sum()),j,i]);
 
-#answer
-ans = pd.DataFrame(columns=['distance','question1','question2']);
-rescp = res.copy();
-while len(rescp) > 1:
-	ri, ci = rescp.stack().idxmin();
-	ans.loc[len(ans),'distance'] = rescp.loc[ri,ci];
-	ans.loc[len(ans)-1,'question1'] = text[ri];
-	ans.loc[len(ans)-1,'question2'] = text[ci];
-	rescp = rescp.drop(index=[ri,ci],columns=[ri,ci]);
+res = sorted(res,key=lambda x: x[0]);
+
+ans = pd.DataFrame(res,columns=['distance','id1','id2']);
+for i in range(len(ans)):
+	ans.loc[i,'question1'] = text[ans.loc[i,'id1']];
+	ans.loc[i,'question2'] = text[ans.loc[i,'id2']];
+
+
+#разбить на пары
+#res = pd.DataFrame();
+#for i in range(len(svd.columns)):
+#	res[i] = np.NaN;
+#	res.loc[i] = np.NaN;
+#	for j in range(i):
+#		res.loc[i,j] = np.sqrt(np.power(svd[i] - svd[j],2).sum());
+#		res.loc[j,i] = res.loc[i,j];
+#ans = pd.DataFrame(columns=['distance','question1','question2']);
+#rescp = res.copy();
+#while len(rescp) > 1:
+#	ri, ci = rescp.stack().idxmin();
+#	ans.loc[len(ans),'distance'] = rescp.loc[ri,ci];
+#	ans.loc[len(ans)-1,'question1'] = text[ri];
+#	ans.loc[len(ans)-1,'question2'] = text[ci];
+#	rescp = rescp.drop(index=[ri,ci],columns=[ri,ci]);
 
 #ans.to_csv('/Users/evgeny/python/workbook/data/nlpSimTest_output.csv', sep = ';');
 ans.to_excel('/Users/evgeny/python/workbook/data/nlpSimTest_output.xlsx',index=False);
