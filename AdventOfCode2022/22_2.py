cfb7e3cc13fb2925daf8b5982b0a89012101b0f8
Evgenii Khvatov <evgen32cd32@gmail.com>
2022-12-22 19:57:35 +0300

    Tinkering 22.2

--- AdventOfCode2022/22_2.py
+++ AdventOfCode2022/22_2.py
@@ -1,5 +1,5 @@
 area = []
-with open('/Users/evgeny/python/workbook/data/advent2022/advent_22.txt','r') as f:
+with open('C:\\Git\\workbook\\data\\advent_22.txt','r') as f:
     area = f.readlines()
     cmds = area[-1][:-1]
     area = [x[:-1] for x in area[:-2]]
@@ -8,52 +8,167 @@ mx = max([len(x) for x in area])
 for i in range(len(area)):
     area[i] = area[i] + ' '*(mx - len(area[i]))
 
+cubeSize = max(len(area),len(area[0]))//4
 
-cubeSize = len(area)//4
+y = 'y'
+x = 'x'
+f = 'f'
+
+class CubeFace:
+    def __init__(self, coord, dir, ar):
+        self.ar = ar
+        self.coord = coord
+        self.dir = dir
+        self.n = {'t':None,'b':None,'l':None,'r':None}
+        self.cd = {'t':0,'b':0,'l':0,'r':0}
+
+faces = [[] for _ in range(6)]
+
+# +-+-+-+
+# |4|0|5|
+# +-+-+-+
+#   |1|
+#   +-+
+#   |2|
+#   +-+
+#   |3|
+#   +-+
+
+# test: 0
+# real: 0
+start = {y:0, x:area[0].find('.')//cubeSize}
+faces[0] = CubeFace(start,0,[row[start[x]*cubeSize:(start[x]+1)*cubeSize] for row in area[0:cubeSize]])
+
+# test: 0 1 2
+# real: 0 1 2
+for i in range(1,3):
+    coord = {y:i, x:start[x]}
+    faces[i] = CubeFace(coord,0,[row[coord[x]*cubeSize:(coord[x]+1)*cubeSize] for row in area[coord[y]*cubeSize:(coord[y]+1)*cubeSize]])
+
+# test: 0 1 2
+# real: 0 1 2 5
+if area[0][(start[x]+1)*cubeSize] != ' ':
+    coord = {y:0, x:start[x]+1}
+    faces[5] = CubeFace(coord,0,[row[coord[x]*cubeSize:(coord[x]+1)*cubeSize] for row in area[coord[y]*cubeSize:(coord[y]+1)*cubeSize]])
+
+# test: 0 1 2 5
+# real: 0 1 2 5
+if area[2*cubeSize][(start[x]+1)*cubeSize] != ' ':
+    coord = {y:2, x:start[x]+1}
+    faces[5] = CubeFace(coord,2,[row[(coord[x]+1)*cubeSize-1:coord[x]*cubeSize-1:-1] for row in area[(coord[y]+1)*cubeSize-1:coord[y]*cubeSize-1:-1]])
+
+# test: 0 1 2 5
+# real: 0 1 2 5 4
+if area[2*cubeSize][(start[x]-1)*cubeSize] != ' ':
+    coord = {y:2, x:start[x]-1}
+    faces[4] = CubeFace(coord,2,[row[(coord[x]+1)*cubeSize-1::-1] for row in area[(coord[y]+1)*cubeSize-1:coord[y]*cubeSize-1:-1]])
+
+# test: 0 1 2 5 3
+# real: 0 1 2 5 4
+if start[x] == 2:
+    coord = {y:1, x:start[x]-2}
+    faces[3] = CubeFace(coord,2,[row[(coord[x]+1)*cubeSize-1::-1] for row in area[(coord[y]+1)*cubeSize-1:coord[y]*cubeSize-1:-1]])
+
+# test: 0 1 2 5 3 4
+# real: 0 1 2 5 4
+if area[cubeSize][(start[x]-1)*cubeSize] != ' ':
+    coord = {y:1, x:start[x]-1}
+    ar = []
+    for i in range(cubeSize):
+        ar.append('')
+        for j in range(cubeSize):
+            ar[-1] = ar[-1] + area[(coord[y]+1)*cubeSize-1-j][coord[x]*cubeSize+i]
+    faces[4] = CubeFace(coord,3,ar)
+
+# test: 0 1 2 5 3 4
+# real: 0 1 2 5 4 3
+if len(area) > 3*cubeSize:
+    coord = {y:start[y]+3, x:start[x]-1}
+    ar = []
+    for i in range(cubeSize):
+        ar.append('')
+        for j in range(cubeSize):
+            ar[-1] = ar[-1] + area[coord[y]*cubeSize+j][(coord[x]+1)*cubeSize-1-i]
+    faces[3] = CubeFace(coord,1,ar)
+
+for i in range(4):
+    faces[i].n['t'] = (i-1)%4
+    faces[i].n['b'] = (i+1)%4
+    faces[i].n['l'] = 4
+    faces[i].n['r'] = 5
+    faces[i].cd['l'] = i
+    faces[i].cd['r'] = -i
+
+faces[4].n = {'t':3,'b':1,'l':2,'r':0}
+faces[4].cd = {'t':1,'b':1,'l':2,'r':0}
+faces[5].n = {'t':3,'b':-1,'l':0,'r':2}
+faces[5].cd = {'t':-1,'b':1,'l':0,'r':2}
 
 # r d l u
 # 0 1 2 3
 facing = 0
 cf = {'L': -1, 'R': 1}
-coord = [0, area[0].find('.')]
+coord = {f:0, y:0, x:0}
 
 def move(steps, facing, coord):
     delta = {0:(0,1),1:(1,0),2:(0,-1),3:(-1,0)}
     for _ in range(steps):
-        nc = [coord[i] + delta[facing][i] for i in range(2)]
-        if nc[0] < 0 or (facing == 3 and area[nc[0]][nc[1]] == ' '):
-            for i in range(len(area)-1,-1,-1):
-                if area[i][nc[1]] == '.':
-                    coord[0] = i
-                    break
-                if area[i][nc[1]] == '#':
-                    break
-            continue
-        if nc[0] >= len(area) or (facing == 1 and area[nc[0]][nc[1]] == ' '):
-            for i in range(len(area)):
-                if area[i][nc[1]] == '.':
-                    coord[0] = i
-                    break
-                if area[i][nc[1]] == '#':
-                    break
-            continue
-        if nc[1] < 0 or (facing == 2 and area[nc[0]][nc[1]] == ' '):
-            for i in range(len(area[0])-1,-1,-1):
-                if area[nc[0]][i] == '.':
-                    coord[1] = i
-                    break
-                if area[nc[0]][i] == '#':
-                    break
-            continue
-        if nc[1] >= len(area[0]) or (facing == 0 and area[nc[0]][nc[1]] == ' '):
-            for i in range(len(area[0])):
-                if area[nc[0]][i] == '.':
-                    coord[1] = i
-                    break
-                if area[nc[0]][i] == '#':
-                    break
-            continue
-        if area[nc[0]][nc[1]] == '#':
+        nc = {f:coord[f], y: coord[y] + delta[facing][0], x: coord[x] + delta[facing][1]}
+        if nc[y] < 0:
+            nc[f] = faces[coord[f]].n['t']
+            cd = faces[coord[f]].cd['t']
+            facing = (facing + cd) % 4
+            if cd == 0:
+                nc[y] = cubeSize - 1
+            elif cd == 1: # 4 -> 3
+                nc[y] = nc[x]
+                nc[x] = 0
+            else: # 5 -> 3
+                nc[y] = cubeSize - 1 - nc[x]
+                nc[x] = cubeSize - 1
+        if nc[y] >= cubeSize:
+            nc[f] = faces[coord[f]].n['b']
+            cd = faces[coord[f]].cd['b']
+            facing = (facing + cd) % 4
+            if cd == 0:
+                nc[y] = 0
+            elif cd == 1: # 5 -> 1
+                nc[y] = nc[x]
+                nc[x] = cubeSize - 1
+            else: # 4 -> 1
+                nc[y] = cubeSize - 1 - nc[x]
+                nc[x] = 0
+        if nc[x] < 0:
+            nc[f] = faces[coord[f]].n['l']
+            cd = faces[coord[f]].cd['l']
+            facing = (facing + cd) % 4
+            if cd == 0:
+                nc[x] = cubeSize - 1
+            elif cd == 2:
+                nc[x] = 0
+                nc[y] = cubeSize - 1 - nc[y]
+            elif cd == 1: # 1 -> 4
+                nc[x] = cubeSize - 1 - nc[y]
+                nc[y] = cubeSize - 1
+            else: # 3 -> 4
+                nc[x] = nc[y]
+                nc[y] = 0
+        if nc[x] >= cubeSize:
+            nc[f] = faces[coord[f]].n['r']
+            cd = faces[coord[f]].cd['r']
+            facing = (facing + cd) % 4
+            if cd == 0:
+                nc[x] = 0
+            elif cd == 2:
+                nc[x] = cubeSize - 1
+                nc[y] = cubeSize - 1 - nc[y]
+            elif cd == -1: # 1 -> 5
+                nc[x] = nc[y]
+                nc[y] = cubeSize - 1
+            else: # 3 -> 5
+                nc[x] = cubeSize - 1 - nc[y]
+                nc[y] = 0
+        if faces[nc[f]].ar[nc[y]][nc[x]] == '#':
             break
         coord = nc
     return coord, facing
@@ -76,4 +191,21 @@ while True:
     coord, facing = move(int(st), facing, coord)
     facing = (facing + cf[dir]) % 4
 
-print(1000 * (coord[0]+1) + 4*(coord[1]+1) + facing)
+face = faces[coord[f]]
+facing = (facing + face.dir) % 4
+ansCoord = {y:face.coord[y]*cubeSize, x: face.coord[x]*cubeSize}
+
+if face.dir == 0:
+    ansCoord[x] += coord[x]
+    ansCoord[y] += coord[y]
+elif face.dir == 2:
+    ansCoord[x] += cubeSize - 1 - coord[x]
+    ansCoord[y] += cubeSize - 1 - coord[y]
+elif face.dir == 1:
+    ansCoord[x] += cubeSize - 1 - coord[y]
+    ansCoord[y] += coord[x]
+else:
+    ansCoord[x] += coord[y]
+    ansCoord[y] += cubeSize - 1 - coord[x]
+
+print(1000 * (ansCoord[y]+1) + 4*(ansCoord[x]+1) + facing)
---
+++ AdventOfCode2022/22_2_bkp.py
@@ -0,0 +1,107 @@
+area = []
+with open('C:\\Git\\workbook\\data\\advent_22.txt','r') as f:
+    area = f.readlines()
+    cmds = area[-1][:-1]
+    area = [x[:-1] for x in area[:-2]]
+
+mx = max([len(x) for x in area])
+for i in range(len(area)):
+    area[i] = area[i] + ' '*(mx - len(area[i]))
+
+
+cubeSize = len(area)//4
+
+# r d l u
+# 0 1 2 3
+facing = 0
+cf = {'L': -1, 'R': 1}
+coord = [0, area[0].find('.')]
+
+def move(steps, facing, coord):
+    delta = {0:(0,1),1:(1,0),2:(0,-1),3:(-1,0)}
+    for _ in range(steps):
+        nc = [coord[i] + delta[facing][i] for i in range(2)]
+        if nc[1] >= len(area[0]): #1
+            facing = 2
+            nc[1] = cubeSize*2 - 1
+            nc[0] = cubeSize*3 - 1 - nc[0]
+        elif nc[0] < 0:
+            if nc[1] // cubeSize == 1: #2
+                facing = 0
+                nc[0] = cubeSize*2 + nc[1]
+                nc[1] = 0
+            else: #3
+                facing = 3
+                nc[0] = cubeSize*4 - 1
+                nc[1] = nc[1] - cubeSize*2
+        elif nc[0] >= len(area): #4
+            nc[0] = 0
+            nc[1] = cubeSize*2 + nc[1]
+        elif nc[1] < 0:
+            if nc[0] // cubeSize == 2: #5
+                facing = 0
+                nc[1] = cubeSize
+                nc[0] = cubeSize*3 - 1 - nc[0]
+            else: #6
+                facing = 1
+                nc[1] = nc[0] - cubeSize*2
+                nc[0] = 0
+        elif area[nc[0]][nc[1]] == ' ':
+            if facing == 2:
+                if nc[0] // cubeSize == 0: #7
+                    facing = 0
+                    nc[1] = 0
+                    nc[0] = cubeSize*3 - 1 - nc[0]
+                else: #8
+                    facing = 1
+                    nc[1] = cubeSize*2 - 1 - nc[0]
+                    nc[0] = cubeSize*2
+            elif facing == 3: #9
+                facing = 0
+                nc[0] = cubeSize + nc[1]
+                nc[1] = cubeSize
+            elif facing == 1:
+                facing = 2
+                if nc[1] // cubeSize == 2: #10
+                    nc[0] = nc[1] - cubeSize
+                    nc[1] = cubeSize*2 - 1
+                else: #11
+                    nc[0] = nc[1] + cubeSize*2
+                    nc[1] = cubeSize - 1
+            else:
+                if nc[0] // cubeSize == 1: #12
+                    facing = 3
+                    nc[1] = nc[0] + cubeSize
+                    nc[0] = cubeSize - 1
+                elif nc[0] // cubeSize == 2: #13
+                    facing = 2
+                    nc[0] = cubeSize*3 - 1 - nc[0]
+                    nc[1] = cubeSize*3 - 1
+                else: #14
+                    facing = 3
+                    nc[1] = nc[0] - cubeSize*2
+                    nc[0] = cubeSize*3 - 1
+        if area[nc[0]][nc[1]] == '#':
+            break
+        coord = nc
+    return coord, facing
+
+nr = cmds.find('R')
+nl = cmds.find('L')
+while True:
+    if nr < 0 or nl < nr:
+        if nl < 0:
+            coord, facing = move(int(cmds), facing, coord)
+            break
+        dir = 'L'
+        nr -= nl + 1
+        nl = cmds.find('L',nl+1) - nl - 1
+    else:
+        dir = 'R'
+        nl -= nr + 1
+        nr = cmds.find('R',nr+1) - nr - 1
+    st, cmds = cmds.split(dir,1)
+    coord, facing = move(int(st), facing, coord)
+    facing = (facing + cf[dir]) % 4
+
+print(1000 * (coord[0]+1) + 4*(coord[1]+1) + facing)
