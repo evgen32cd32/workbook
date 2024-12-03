dafc2957c4cd55504662df742ec55a8167890a99
Evgenii Khvatov <evgen32cd32@gmail.com>
2024-12-03 08:48:45 +0300

    AoC2023 day17

--- AoC/2023/17.py
+++ AoC/2023/17.py
@@ -1,51 +1,90 @@
-import sys
-sys.setrecursionlimit(10000000)
+from math import inf
 
-with open('/Users/evgeny/python/workbook/data/advent2023/advent_17.txt','r') as f:
-    ar = []
-    hm = []
+ar = []
+with open('/Users/evgen/projects/workbook/data/advent2023/advent_17.txt','r') as f:
     for line in f.readlines():
         ar.append([int(x) for x in line if x.isdigit()])
-        hm.append([])
-        for _ in range(len(ar[0])):
-            hm[-1].append({})
+    
+drs = {'u':['l','r'],
+       'd':['r','l'],
+       'l':['d','u'],
+       'r':['u','d']}
 
-    queue = set()
+def task(mins,maxs):    
+    hm = []
+    for _ in range(len(ar)):
+        hm.append([])
+        for __ in range(len(ar[0])):
+            hm[-1].append({})
     
-    def rec(i,j,way,steps):
-        def check(h,a,b,w,s):
-            if a == len(ar)-1 and b == len(ar)-1:
-                h.append(ar[a][b])
-            elif (w,s) in hm[a][b]:
-                h.append(hm[a][b][(w,s)])
+    for way in drs:
+        hm[0][0][way] = {maxs-mins:0}
+
+    ar1 = [(0,0,0,'r',maxs-mins),(0,0,0,'d',maxs-mins)]
+    ar2 = []
+
+    ans1 = inf
+
+    def check(nh,ni,nj,way,ns):
+        if nh >= ans1:
+            return []
+        
+        out = []
+        d = hm[ni][nj]
+
+        for w,s in zip([way]+drs[way],[ns-1,maxs-mins,maxs-mins]):
+            if s < 0:
+                continue
+            if w not in d:
+                d[w] = {s:nh}
+                out.append((w,s))
             else:
-                if (a,b,w,s) not in queue:
-                    
-                    h.append(rec(a,b,w,s))
-            return h
-        heats = []
-        if steps < 3:
-            if way == 'u' and i > 0:
-                heats = check(heats,i-1,j,'u',steps+1)
-            if way == 'd' and i < len(ar)-1:
-                heats = check(heats,i+1,j,'d',steps+1)
-            if way == 'l' and j > 0:
-                heats = check(heats,i,j-1,'l',steps+1)
-            if way == 'r' and j < len(ar[0])-1:
-                heats = check(heats,i,j+1,'r',steps+1)
-        if way == 'l' or way == 'r':
-            if i > 0:
-                heats = check(heats,i-1,j,'u',1)
-            if i < len(ar)-1:
-                heats = check(heats,i+1,j,'d',1)
-        if way == 'u' or way == 'd':
-            if j > 0:
-                heats = check(heats,i,j-1,'l',1)
-            if j < len(ar[0])-1:
-                heats = check(heats,i,j+1,'r',1)
-        heat = ar[i][j] + min(heats)
-        hm[i][j][(way,steps)] = heat
-        return heat
-    
+                fl = True
+                for a in range(maxs-mins,s-1,-1):
+                    if a in d[w] and d[w][a] <= nh:
+                        fl = False
+                        break
+                if fl:
+                    d[w][s] = nh
+                    out.append((w,s))
+        return out
+
+        
+    while len(ar1) > 0:
+        for heat,i,j,way,steps in ar1:
+            if i == len(ar)-1 and j == len(ar[0])-1:
+                ans1 = min([heat,ans1])
+            step = mins if steps == maxs-mins else 1
+            ni = i
+            nj = j
+            nh = heat
+            if way == 'u' and i-step >= 0:
+                ni = i-step
+                for a in range(1,step+1):
+                    nh += ar[i-a][j]
+            elif way == 'd' and i+step < len(ar):
+                ni = i+step
+                for a in range(1,step+1):
+                    nh += ar[i+a][j]
+            elif way == 'l' and j-step >= 0:
+                nj = j-step
+                for a in range(1,step+1):
+                    nh += ar[i][j-a]
+            elif way == 'r' and j+step < len(ar[0]):
+                nj = j+step
+                for a in range(1,step+1):
+                    nh += ar[i][j+a]
+            else:
+                continue
+
+            for w,s in check(nh,ni,nj,way,steps):
+                ar2.append((nh,ni,nj,w,s))
+        ar1 = ar2
+        ar2 = []
+    return ans1
+
 # first answer
-print(rec(0,0,'r',0))
+print(task(1,3))
+
+# second answer
+print(task(4,10))
